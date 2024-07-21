31a7217ce4344032e18089ca4c111aa041f753fc
rizki <rizkiadnanfutuh@gmail.com>
2024-07-21 22:36:05 +0700

    feat : add func to choice the gender

--- lib/components/icon_text.dart
+++ lib/components/icon_text.dart
@@ -3,27 +3,33 @@ import 'package:flutter/material.dart';
 class IconText extends StatelessWidget {
   final IconData icon;
   final String txt;
+  final void Function() onTap;
+
   const IconText({
     super.key,
     required this.icon,
     required this.txt,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          color: Colors.white,
-          size: 70,
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-        Text(txt)
-      ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 70,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(txt)
+        ],
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 double bottomContainerHeight = 80.0;
-Color darkPurple = const Color(0XFF1D1E33);
+
+Color darkPurple1 = const Color.fromRGBO(36, 37, 65, 1);
+Color darkPurple2 = const Color(0XFF1D1E33);
 Color darkPink = const Color(0xffeb1555);
+
+enum Gender { male, female }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +21,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  Color activeButton = darkPurple2;
+  Color inActiveButton = darkPurple1;
+
+  Gender? isActive;
+
+  void buttonActive(Gender gender) {
+    setState(() {
+      isActive = gender;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,15 +47,23 @@ class _HomePage extends State<HomePage> {
             child: Row(
               children: [
                 Box(
-                  color: darkPurple,
-                  boxChild: const IconText(
+                  color:
+                      isActive == Gender.male ? activeButton : inActiveButton,
+                  boxChild: IconText(
+                    onTap: () {
+                      buttonActive(Gender.male);
+                    },
                     icon: FontAwesomeIcons.mars,
                     txt: 'MALE',
                   ),
                 ),
                 Box(
-                  color: darkPurple,
-                  boxChild: const IconText(
+                  color:
+                      isActive == Gender.female ? activeButton : inActiveButton,
+                  boxChild: IconText(
+                    onTap: () {
+                      buttonActive(Gender.female);
+                    },
                     icon: FontAwesomeIcons.venus,
                     txt: 'FEMALE',
                   ),
@@ -50,7 +73,7 @@ class _HomePage extends State<HomePage> {
           ),
           Expanded(
             child: Box(
-              color: darkPurple,
+              color: darkPurple1,
               boxChild: null,
             ),
           ),
@@ -58,11 +81,11 @@ class _HomePage extends State<HomePage> {
             child: Row(
               children: [
                 Box(
-                  color: darkPurple,
+                  color: darkPurple1,
                   boxChild: null,
                 ),
                 Box(
-                  color: darkPurple,
+                  color: darkPurple1,
                   boxChild: null,
                 ),
               ],
