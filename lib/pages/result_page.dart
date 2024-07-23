37599ba3d33ad02e13ba561b8e431abccf31864a
rizki <rizkiadnanfutuh@gmail.com>
2024-07-23 15:58:34 +0700

    feat : add result value and conclution

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
+import '../theme/theme.dart';
+
 class ResultPage extends StatelessWidget {
   final double weight;
   final double height;
@@ -8,28 +10,68 @@ class ResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String calculate(double weight, double height) {
+    double calculate(double weight, double height) {
       Parser p = Parser();
       String equation = '${weight.round()}/((${height.round() / 100})^2)';
       Expression exp = p.parse(equation);
       double result = exp.evaluate(EvaluationType.REAL, ContextModel());
 
-      return result.toString();
+      return result;
     }
 
-    String result = calculate(weight, height);
+    double result = calculate(weight, height);
+    BMIConclution conclution() {
+      switch (result) {
+        case < 18.5:
+          return BMIConclution('UNDERWEIGHT', MyTheme().warningResult);
+        case > 18.5 && < 25:
+          return BMIConclution('NORMAL', MyTheme().normalResult);
+        case > 25 && < 29:
+          return BMIConclution('OVERWEIGHT', MyTheme().warningResult);
+        case > 29:
+          return BMIConclution('OBESITY', MyTheme().dangerResult);
 
-// Underweight = <18.5
-// Normal weight = 18.5–24.9
-// Overweight = 25–29.9
-// Obesity = BMI of 30 or greater
+        default:
+          return BMIConclution('LOADING', MyTheme().normalResult);
+      }
+    }
 
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
+      ),
       body: Center(
-        child: Text(
-          result,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'RESULT',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              result.toStringAsPrecision(3),
+              style: MyTheme().valTxtStyle,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              conclution().conclution,
+              style: conclution().textStyle,
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class BMIConclution {
+  final String conclution;
+  final TextStyle textStyle;
+  const BMIConclution(this.conclution, this.textStyle);
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,9 +1,11 @@
+import 'package:bmi_medium/constans/color.dart';
 import 'package:flutter/material.dart';
 
 class MyTheme {
   ThemeData get theme => ThemeData.dark().copyWith(
         //copy with = use all of dark them and change some part of it
         appBarTheme: const AppBarTheme(
+          centerTitle: true,
           titleTextStyle: TextStyle(
             fontFamily: 'Poppins',
             fontSize: 17,
@@ -33,5 +35,24 @@ class MyTheme {
         fontSize: 40,
         fontWeight: FontWeight.w900,
         letterSpacing: 1.5,
+      );
+  //------ result page
+  TextStyle get normalResult => const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w900,
+        letterSpacing: 2.0,
+        color: normal,
+      );
+  TextStyle get warningResult => const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w900,
+        letterSpacing: 2.0,
+        color: warning,
+      );
+  TextStyle get dangerResult => const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w900,
+        letterSpacing: 2.0,
+        color: danger,
       );
 }
